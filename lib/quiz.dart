ee278c32f74e4976a0abb88d58c210e36d049b26
Nikolas Pateras <nikolas.98@live.com>
2023-09-20 16:21:15 +0300

    Initial commit

---
+++ lib/quiz.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    super.initState();
+    activeScreen = StartScreen(switchScreen);
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color.fromARGB(255, 142, 33, 216),
+                Color.fromARGB(255, 99, 8, 158),
+              ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight)),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
